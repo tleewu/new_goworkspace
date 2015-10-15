6b6f5499a798a80c0215e66ac487dd13117ead26
Theo <tleewu@ucla.edu>
2015-10-14 17:49:35 -0700

    touched up with some login styling.

--- app/assets/javascripts/workspace.js.jsx
+++ app/assets/javascripts/workspace.js.jsx
@@ -4,11 +4,15 @@ $(function() {
   var Router = ReactRouter.Router;
   var Route = ReactRouter.Route;
   // var IndexRoute = ReactRouter.IndexRoute;
+  var divStyle = {
+    backgroundImage: 'none',
+    backgroundColor: 'red'
+  };
 
   var App = React.createClass({
     render: function () {
       return (
-        <div>
+        <div style={divStyle}>
           <NavBar />
           {this.props.children}
         </div>
@@ -25,4 +29,6 @@ $(function() {
 
   React.render(<Router>{routes}</Router>,
                 document.getElementById("workspace"));
+                
+  $("body").css("background-image", "none");
 });
