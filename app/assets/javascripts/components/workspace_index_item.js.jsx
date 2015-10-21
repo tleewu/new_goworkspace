0368541f6ba872adf6e6ff8e2086ef4ae8e2fd88
Theo <tleewu@ucla.edu>
2015-10-20 20:13:23 -0700

    small edit

--- app/assets/javascripts/components/workspace_index_item.js.jsx
+++ app/assets/javascripts/components/workspace_index_item.js.jsx
@@ -23,7 +23,7 @@
               </div>
               <div className="col-md-2">
                 <div> Overall: {workspace.overall} </div>
-                <div> Seating: {workspace.seating} Hours: {workspace.weekday_opening} {workspace.weekday_closing} </div>
+                <div> Seating: {workspace.seating} </div>
               </div>
             </div>
           </div>
