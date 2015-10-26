6c709d35bc11d717e26e15fa74ccbf39369adfef
Theo <tleewu@ucla.edu>
2015-10-25 17:09:14 -0700

    config serve static assets true

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module Theofinalapp
     config.time_zone = 'Pacific Time (US & Canada)'
     config.active_record.default_timezone = :local
 
+    config.serve_static_assets = true
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
